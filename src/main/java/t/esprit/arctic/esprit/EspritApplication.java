e0f72507af8cfbcba6cbb7b1875422760b668a54
nesrinelamine2021@gmail.com <nesrinelamine2021@gmail.com>
2026-02-05 10:01:28 -1100

    base de donnes

---
+++ src/main/java/t/esprit/arctic/esprit/EspritApplication.java
@@ -0,0 +1,13 @@
+package t.esprit.arctic.esprit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspritApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EspritApplication.class, args);
+    }
+
+}
---
+++ src/test/java/t/esprit/arctic/esprit/EspritApplicationTests.java
@@ -0,0 +1,13 @@
+package t.esprit.arctic.esprit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspritApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
